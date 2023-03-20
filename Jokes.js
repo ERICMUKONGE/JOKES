46def1e712955d2cba17dd6d1f100e64e535c1a3
MUKONGE ERIC <mukonge.eric@gmail.com>
2023-03-20 17:14:27 +0300

    First commit

---
+++ Jokes.js
@@ -0,0 +1,13 @@
+// JOKES
+var joke = "JavaScript walked into a bar....";
+var toldJoke = false;
+var $punchline ="Better watch out for those semi-colons.";
+var perentage = 20;
+var result;
+if (toldJoke == true) {
+alert($punchline);
+}
+else
+{
+alert(joke);
+}
